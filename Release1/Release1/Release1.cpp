12cb548933ca85a27dad0c104b9a97d25f56b71b
pastilkaxo <98273582+pastilkaxo@users.noreply.github.com>
2022-12-12 23:10:01 +0300

    Загружен Realease1/patch1

---
+++ Release1/Release1/Release1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int number;
+	cout << "Выберите проект который хотите посмотреть:" << endl;
+	cout << "1 - " << endl;
+	cout << "2 - ' " << endl;
+	cout << "3 - " << endl;
+    cout << "3 - " << endl;
+	cin >> number;
+
+
+	switch (number)
+	{
+	case '1':
+      
+		break;
+	case '2':
+		break;
+	case '3':
+		break;
+	case '4':
+		break;
+	}
+	
+
+
+
+
+
+	return 0;
+}
