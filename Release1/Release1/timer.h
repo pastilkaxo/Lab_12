55febd4d3f611546de103b036e37565c5b25f404
ascoltat0 <118727291+LexicalReduplication@users.noreply.github.com>
2022-12-26 18:46:51 +0300

    Add files via upload

--- Release1/Release1/timer.h
+++ Release1/Release1/timer.h
@@ -5,8 +5,8 @@
 using namespace std;
 
 void displayClock(int hours, int minutes, int seconds) {
-	system("cls");
-	cout << setfill(' ') << setw(49) << "Обратный таймер\n";
+	system("cls"); // Очистка консоли
+	cout << setfill(' ') << setw(50) << "Обратный таймер\n";
 	cout << setfill(' ') << setw(55) << " ==========================\n";
 	cout << setfill(' ') << setw(29);
 	cout << "| " << setfill('0') << setw(2) << hours << " hrs / ";
@@ -17,32 +17,33 @@ void displayClock(int hours, int minutes, int seconds) {
 
 void timer(int hours, int minutes, int seconds) {
 	while (true) {
-		displayClock(hours, minutes, seconds);
-		Sleep(1000);
-		seconds--;
+		displayClock(hours, minutes, seconds); // Выводим время на экран
+		Sleep(1000); // Приостанавливаем выполнение программы на 1 секунду
+		seconds--; // Уменьшаем секунды на 1
 		if (seconds == 0) {
-			if (hours == 0 && minutes == 0 && seconds == 0) {
+			if (hours == 0 && minutes == 0 && seconds == 0) {// Если время закончилось
 				displayClock(hours, minutes, 0);
 				cout << "Время вышло" << endl;
 				return;
 			}
-			else if (minutes == 0 && seconds == 0) {
-				hours--;
+			else if (minutes == 0 && seconds == 0) {// Если минуты и секунды закончились
+				hours--; // Уменьшаем часы на 1
 				minutes = 59;
 				seconds = 59;
 			}
-			else if (seconds == 0) {
-				minutes--;
+			else if (seconds == 0) {// Если секунды закончились
+				minutes--; // Уменьшаем минуты на 1
 				seconds = 59;
 			}
 		}
 	}
 }
 
-void timerOutput() {
+int timerOutput() {
+	system("cls");
 	setlocale(LC_ALL, "RUS");
 	int hours, minutes, seconds;
-	system("cls");
+	// Проверка на корректность ввода
 	do {
 		cout << "Введите часы: ";
 		cin >> hours;
@@ -56,5 +57,6 @@ void timerOutput() {
 		cin >> seconds;
 	} while (seconds < 0);
 
-	timer(hours, minutes, seconds);
+	timer(hours, minutes, seconds); // Запускаем таймер
+	return 0;
 }
